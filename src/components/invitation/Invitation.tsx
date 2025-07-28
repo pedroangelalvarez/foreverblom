3d99475255a74250c3b9931533e19e1adffa805f
pedroangelalvarez <pedroangel.alvarez24@gmail.com>
2025-07-28 00:58:56 -0500

    UPDATE: Fix font names

--- src/components/invitation/Invitation.tsx
+++ src/components/invitation/Invitation.tsx
@@ -5,29 +5,37 @@ import type { GuestData } from "@/types";
 
 import { useRef } from "react";
 import html2canvas from "html2canvas";
-import { Camera } from "lucide-react";
+import { Camera, MapPin, Utensils } from "lucide-react";
 
 export default function Invitation({ guestData }: { guestData: GuestData | null }) {
   const captureRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
   const handleDownload = async () => {
-    const button = buttonRef.current;
-    if (button) {
-      button.style.display = 'none';
-    }
     if (captureRef.current) {
-      const canvas = await html2canvas(captureRef.current);
+      // Clonar el elemento a capturar
+      const elementToCapture = captureRef.current.cloneNode(true) as HTMLElement;
+
+      // Eliminar el botón del clon
+      const buttonInClone = elementToCapture.querySelector('button');
+      if (buttonInClone) {
+        buttonInClone.remove();
+      }
+
+      // Añadir el clon al cuerpo del documento para que se renderice
+      document.body.appendChild(elementToCapture);
+
+      const canvas = await html2canvas(elementToCapture);
       const image = canvas.toDataURL('image/png');
 
-      // Crea un enlace de descarga
+      // Eliminar el clon del cuerpo del documento
+      document.body.removeChild(elementToCapture);
+
+      // Crear un enlace de descarga
       const link = document.createElement('a');
       link.href = image;
       link.download = 'captura.png';
       link.click();
-    }
-    if (button) {
-      button.style.display = '';
     }
   };
 
@@ -62,7 +70,13 @@ export default function Invitation({ guestData }: { guestData: GuestData | null 
           <p className={styles.month}>DE AGOSTO DE 2025</p>
 
           <p className={styles.address}>
+            <MapPin className="inline-block mr-2 h-5 w-5" />
             JR. COLÓN N°744, ESQUINA CON JR. AYACUCHO, EN EL CENTRO HISTÓRICO DE TRUJILLO
+          </p>
+
+          <p className={styles.address}>
+            <Utensils className="inline-block mr-2 h-5 w-5" />
+            Recepción: AV. LARCO 834
           </p>
 
           <p className={`${styles.church} ${styles.family}`}>
@@ -71,7 +85,7 @@ export default function Invitation({ guestData }: { guestData: GuestData | null 
         <button
           ref={buttonRef}
           onClick={handleDownload}
-          className="mt-4 flex items-center gap-2 px-4 py-2 bg-white/20 text-white rounded-full mx-auto backdrop-blur-lg border border-white/20 shadow-lg shadow-black/10 [text-shadow:_0_1px_1px_rgba(0,0,0,0.5)]"
+          className="mt-4 flex items-center gap-2 px-4 py-2 text-white rounded-full mx-auto backdrop-blur-3xl border border-white/30 shadow-xl shadow-black/20 [box-shadow:inset_0_1px_1px_rgba(255,255,255,0.5)] [text-shadow:_0_1px_1px_rgba(0,0,0,0.5)]"
         >
           <Camera className="h-5 w-5 drop-shadow-md" />
           Descargar Invitacion
